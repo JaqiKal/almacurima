151a20cb86b946d1aaa483218e9461a638fe4c0b
JaqiKal <jaqika@gmail.com>
2025-05-04 18:18:28 +0200

    style: define secondary default color in theme
    - Adds 'secondary.DEFAULT' for use as base background color
    - Enables usage of 'bg-secondary' and 'text-secondary' in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,20 @@ export default {
     extend: {
       colors: {
         primary: "#ffffff",       // Background
-        secondary: "#e3e4e0",     // Sections, grids etc
         heading: "#0e0e0e",       // Text
+        secondary: {
+          DEFAULT: "#e3e4e0", // bg-secondary
+          "50": "#fcfcfc",
+          "100": "#f9faf9",
+          "200": "#f4f4f3",
+          "300": "#eeefec",
+          "400": "#e9e9e6",
+          "500": "#e3e4e0",
+          "600": "#b6b6b3",
+          "700": "#888986",
+          "800": "#5b5b5a",
+          "900": "#2d2e2d"
+        },
       },
     },
   },
