b33834dc594e1fec9bdab4f2e2c748cec361bf7b
JaqiKal <jaqika@gmail.com>
2025-05-04 18:39:26 +0200

    chore: configure GitHub Pages deployment
    - Adds gh-pages as dev dependency
    - Sets 'base' in vite.config.js to match repo name
    - Adds deploy script using gh-pages to publish /dist

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 // main.jsx
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./style/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
