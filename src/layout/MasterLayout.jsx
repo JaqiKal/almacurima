1325b0667c031a08b719740186a176bf28732e6f
JaqiKal <jaqika@gmail.com>
2025-05-04 12:41:49 +0200

    feat: create MasterLayout
    - Basic layout with bg color and min height

---
+++ src/layout/MasterLayout.jsx
@@ -0,0 +1,8 @@
+// 📁 src/layout/MasterLayout.jsx
+import React from "react";
+
+function MasterLayout({ children }) {
+  return <div className="bg-white text-[#0e0e0e] min-h-screen p-4">{children}</div>;
+}
+
+export default MasterLayout;
