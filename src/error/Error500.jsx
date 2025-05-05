d09486ae02688e88a744d3ec8c4c98cb1a7a1413
JaqiKal <jaqika@gmail.com>
2025-05-05 20:02:03 +0200

    fix(Error500): Correct code from 403 to 500.

--- src/error/Error500.jsx
+++ src/error/Error500.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ErrorPage from "../components/common/ErrorPage";
 
 function Error500() {
-  return <ErrorPage code={403} message="Error 500. An unexpected server error occurred." />;
+  return <ErrorPage code={500} message="Error 500. An unexpected server error occurred." />;
 }
 
 export default Error500;
